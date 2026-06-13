7befaa4ddfea6e642fdcbd2f6a288318870fd73f
Mohammadreza Mokarrami <mohammadreza.mokarrami@ososoft.de>
2026-06-13 21:13:25 +0200

    Routing Fixed - Some Logic and error Handling added - Made some Fields dynamic

--- webapp/controller/Create.controller.js
+++ webapp/controller/Create.controller.js
@@ -2,77 +2,240 @@ sap.ui.define([
   "sap/ui/core/mvc/Controller",
   "sap/ui/model/json/JSONModel",
   "sap/m/MessageToast",
-  "sap/m/MessageBox"
-], function (Controller, JSONModel, MessageToast, MessageBox) {
+  "sap/m/MessageBox",
+  "sap/ui/core/Fragment"
+], function (Controller, JSONModel, MessageToast, MessageBox, Fragment) {
   "use strict";
 
   function fmtEuro(n) {
-    return n.toLocaleString("de-DE", { minimumFractionDigits: 2, maximumFractionDigits: 2 }) + " €";
+    return n.toLocaleString("de-DE", {
+      minimumFractionDigits: 2, maximumFractionDigits: 2
+    }) + " €";
   }
 
   function emptyCreate() {
     return {
-      lieferant:"", kategorie:"", bestelldatum:"", lieferdatum:"",
-      einkaeufer:"", prioritaet:"Mittel", zahlung:"Netto 30 Tage",
-      lieferadresse:"", bemerkungen:"", gesamtbetrag:"0,00 €",
-      positions:[{ mat:"", name:"", qty:0, unit:"Stk", price:0 }]
+      lieferant: "", kategorie: "", bestelldatum: "", lieferdatum: "",
+      einkaeufer: "", prioritaet: "Mittel", zahlung: "Netto 30 Tage",
+      lieferadresse: "", bemerkungen: "", gesamtbetrag: "0,00 €",
+      positions: [{ mat: "", name: "", qty: 0, unit: "", price: 0 }]
     };
+  }
+
+  function generateFakePONumber() {
+    return String(4500002416 + Math.floor(Math.random() * 9999));
   }
 
   return Controller.extend("purchaseorders.controller.Create", {
 
-    onInit: function () {
-      this.getView().setModel(new JSONModel(emptyCreate()), "create");
-      // reset form each time the create route is opened
-      var oRouter = this.getOwnerComponent().getRouter();
-      oRouter.getRoute("create").attachPatternMatched(this._onRouteMatched, this);
+    init: function (oView, oODataModel) {
+      this._oView = oView;
+      this._oDialog = null;
+
+      oView.setModel(new JSONModel(emptyCreate()), "create");
+      oView.setModel(new JSONModel([{ key: "", text: "Wird geladen..." }]), "suppliers");
+      oView.setModel(new JSONModel([
+        { key: "", text: "Bitte wählen..." },
+        { key: "Stk", text: "Stk" },
+        { key: "kg", text: "kg" },
+        { key: "m", text: "m" },
+        { key: "Pack", text: "Pack" },
+        { key: "Set", text: "Set" }
+      ]), "units");
+
+      oODataModel.metadataLoaded().then(function () {
+        this._loadSuppliers(oODataModel);
+        this._loadUnits(oODataModel);
+      }.bind(this));
     },
 
-    _onRouteMatched: function () {
-      this.getView().getModel("create").setData(emptyCreate());
+    openDialog: function () {
+      this._oView.getModel("create").setData(emptyCreate());
+
+      var oLieferPicker = Fragment.byId(this._oView.getId(), "cLieferdatum");
+      if (oLieferPicker) {
+        oLieferPicker.setValueState("None");
+        oLieferPicker.setValueStateText("");
+        oLieferPicker.setMinDate(null);
+      }
+
+      if (this._oDialog) {
+        this._oDialog.open();
+        return;
+      }
+
+      Fragment.load({
+        id: this._oView.getId(),
+        name: "purchaseorders.view.Create",
+        controller: this
+      }).then(function (oDialog) {
+        this._oView.addDependent(oDialog);
+        this._oDialog = oDialog;
+        oDialog.open();
+      }.bind(this)).catch(function (oError) {
+        MessageBox.error("Fragment konnte nicht geladen werden:\n" + oError.message);
+        console.error(oError);
+      });
     },
 
-    onCancel: function () {
-      this.getOwnerComponent().getRouter().navTo("list");
+    onCancelCreate: function () {
+      if (this._oDialog) { this._oDialog.close(); }
     },
 
     onAddPosition: function () {
-      var oModel = this.getView().getModel("create");
+      var oModel = this._oView.getModel("create");
       var aPos = oModel.getProperty("/positions");
-      aPos.push({ mat:"", name:"", qty:0, unit:"Stk", price:0 });
+      aPos.push({ mat: "", name: "", qty: 0, unit: "", price: 0 });
       oModel.setProperty("/positions", aPos);
+    },
+
+    onDateChange: function () {
+      var oModel = this._oView.getModel("create");
+      var sBestelldatum = oModel.getProperty("/bestelldatum");
+      var sLieferdatum = oModel.getProperty("/lieferdatum");
+
+      var oLieferPicker = Fragment.byId(this._oView.getId(), "cLieferdatum");
+      if (!oLieferPicker) { return; }
+
+      var fnParse = function (s) {
+        var p = s.split(".");
+        return new Date(parseInt(p[2], 10), parseInt(p[1], 10) - 1, parseInt(p[0], 10));
+      };
+
+      if (sBestelldatum) {
+        var oMin = fnParse(sBestelldatum);
+        if (!isNaN(oMin.getTime())) {
+          oLieferPicker.setMinDate(oMin);
+        }
+      }
+
+      if (!sBestelldatum || !sLieferdatum) {
+        oLieferPicker.setValueState("None");
+        return;
+      }
+
+      var oBestellDate = fnParse(sBestelldatum);
+      var oLieferDate = fnParse(sLieferdatum);
+
+      if (isNaN(oBestellDate.getTime()) || isNaN(oLieferDate.getTime())) {
+        oLieferPicker.setValueState("None");
+        return;
+      }
+
+      if (oLieferDate < oBestellDate) {
+        oLieferPicker.setValueState("Error");
+        oLieferPicker.setValueStateText(
+          "Lieferdatum darf nicht vor dem Bestelldatum liegen."
+        );
+      } else {
+        oLieferPicker.setValueState("None");
+        oLieferPicker.setValueStateText("");
+      }
     },
 
     onRemovePosition: function (oEvent) {
-      var oModel = this.getView().getModel("create");
+      var oModel = this._oView.getModel("create");
       var aPos = oModel.getProperty("/positions");
-      var iIdx = parseInt(oEvent.getSource().getBindingContext("create").getPath().split("/").pop(), 10);
+      var iIdx = parseInt(
+        oEvent.getSource().getBindingContext("create").getPath().split("/").pop(), 10
+      );
       aPos.splice(iIdx, 1);
       oModel.setProperty("/positions", aPos);
-      this.onPositionChange();
+      this.onPositionChange();   
     },
 
-    onPositionChange: function () {
-      var oModel = this.getView().getModel("create");
+    onPositionChange: function (oEvent) {
+      var oModel = this._oView.getModel("create");
+
+      if (oEvent && oEvent.getSource) {
+        var oSource = oEvent.getSource();
+        var sNewVal = oEvent.getParameter("newValue");
+        var oCtx = oSource.getBindingContext("create");
+        if (oCtx && sNewVal !== undefined) {
+          var sField = oSource.getBinding("value").getPath();
+          oModel.setProperty(oCtx.getPath() + "/" + sField, sNewVal);
+        }
+      }
+
       var aPos = oModel.getProperty("/positions");
       var total = 0;
-      aPos.forEach(function(p) {
+      aPos.forEach(function (p) {
         total += (parseFloat(p.qty) || 0) * (parseFloat(p.price) || 0);
       });
       oModel.setProperty("/gesamtbetrag", fmtEuro(total));
     },
 
-    onSave: function () {
-      var oData = this.getView().getModel("create").getData();
+    onSaveCreate: function () {
+      var oData = this._oView.getModel("create").getData();
 
       if (!oData.lieferant || !oData.kategorie || !oData.bestelldatum ||
-          !oData.lieferdatum || !oData.einkaeufer || !oData.lieferadresse) {
+        !oData.lieferdatum || !oData.einkaeufer || !oData.lieferadresse) {
         MessageBox.error("Bitte alle Pflichtfelder ausfüllen.");
         return;
       }
 
-      MessageToast.show("Bestellung wurde erstellt!");
-      this.getOwnerComponent().getRouter().navTo("list");
+      var fnParse = function (s) {
+        var p = s.split(".");
+        return new Date(parseInt(p[2], 10), parseInt(p[1], 10) - 1, parseInt(p[0], 10));
+      };
+
+      if (fnParse(oData.lieferdatum) < fnParse(oData.bestelldatum)) {
+        MessageBox.error("Lieferdatum darf nicht vor dem Bestelldatum liegen.");
+        return;
+      }
+
+      if (this._oDialog) { this._oDialog.close(); }
+
+      MessageBox.success(
+        "Die Bestellung wurde erfolgreich angelegt.\n\nBestellnummer: " + generateFakePONumber(),
+        { title: "Bestellung erstellt" }
+      );
+    },
+
+    _loadSuppliers: function (oModel) {
+      oModel.read("/ZOSO_C_PurchaseOrder", {
+        urlParameters: { "$select": "Supplier", "$top": "500" },
+        success: function (oData) {
+          var oSeen = {};
+          var aList = [{ key: "", text: "Bitte wählen..." }];
+          oData.results.forEach(function (item) {
+            if (item.Supplier && !oSeen[item.Supplier]) {
+              oSeen[item.Supplier] = true;
+              aList.push({ key: item.Supplier, text: item.Supplier });
+            }
+          });
+          aList.sort(function (a, b) {
+            if (!a.key) { return -1; }
+            if (!b.key) { return 1; }
+            return a.key.localeCompare(b.key);
+          });
+          this._oView.getModel("suppliers").setData(aList);
+        }.bind(this),
+        error: function () {
+          this._oView.getModel("suppliers").setData([{ key: "", text: "Bitte wählen..." }]);
+          MessageToast.show("Lieferanten konnten nicht geladen werden.");
+        }.bind(this)
+      });
+    },
+
+    _loadUnits: function (oModel) {
+      oModel.read("/SAP__UnitsOfMeasure", {
+        urlParameters: { "$select": "UnitCode,ExternalCode,Text" },
+        success: function (oData) {
+          var aList = [{ key: "", text: "Bitte wählen..." }];
+          oData.results.forEach(function (item) {
+            var sDisplay = (item.ExternalCode && item.ExternalCode.trim())
+              ? item.ExternalCode.trim() : item.UnitCode;
+            if (item.UnitCode && sDisplay) {
+              aList.push({ key: item.UnitCode, text: sDisplay });
+            }
+          });
+          this._oView.getModel("units").setData(aList);
+        }.bind(this),
+        error: function () {
+          MessageToast.show("Einheiten konnten nicht geladen werden.");
+        }.bind(this)
+      });
     }
 
   });
--- webapp/controller/List.controller.js
+++ webapp/controller/List.controller.js
@@ -1,183 +1,129 @@
-sap.ui.define(
-  [
-    "sap/ui/core/mvc/Controller",
-    "sap/ui/model/Filter",
-    "sap/ui/model/FilterOperator",
-    "sap/ui/model/json/JSONModel",
-    "sap/m/MessageToast",
-    "sap/m/MessageBox"
-  ],
-  function (Controller, Filter, FilterOperator, JSONModel, MessageToast, MessageBox) {
-    "use strict";
+sap.ui.define([
+  "sap/ui/core/mvc/Controller",
+  "sap/ui/model/Filter",
+  "sap/ui/model/FilterOperator",
+  "sap/m/MessageToast",
+  "sap/m/MessageBox",
+    "purchaseorders/controller/Create.controller"          
+], function (Controller, Filter, FilterOperator, MessageToast, MessageBox, CreateController) {
+  "use strict";
 
-    var STATUS_TEXT = {
-      "01": "Version in Arbeit", "02": "Aktiv", "03": "In Freigabe",
-      "04": "Freigabe teilweise", "05": "Freigabe abgeschlossen", "08": "Abgelehnt",
-      11: "In Verteilung", 12: "Fehler bei Verteilung", 13: "Verteilt",
-      14: "In Vorbereitung", 26: "In externer Genehmigung"
-    };
+  var STATUS_TEXT = {
+    "01": "Version in Arbeit",   "02": "Aktiv",
+    "03": "In Freigabe",         "04": "Freigabe teilweise",
+    "05": "Freigabe abgeschlossen", "08": "Abgelehnt",
+    "11": "In Verteilung",       "12": "Fehler bei Verteilung",
+    "13": "Verteilt",            "14": "In Vorbereitung",
+    "26": "In externer Genehmigung"
+  };
 
-    var STATUS_STATE = {
-      "01": "None", "02": "Success", "03": "Warning", "04": "Warning",
-      "05": "Success", "08": "Error", 11: "Information", 12: "Error",
-      13: "Success", 14: "None", 26: "Warning"
-    };
+  var STATUS_STATE = {
+    "01": "None",    "02": "Success", "03": "Warning",
+    "04": "Warning", "05": "Success", "08": "Error",
+    "11": "Information", "12": "Error", "13": "Success",
+    "14": "None",    "26": "Warning"
+  };
 
-    function fmtEuro(n) {
-      return n.toLocaleString("de-DE", { minimumFractionDigits: 2, maximumFractionDigits: 2 }) + " €";
-    }
+  return Controller.extend("purchaseorders.controller.List", {
 
-    function emptyCreate() {
-      return {
-        lieferant: "", kategorie: "", bestelldatum: "", lieferdatum: "",
-        einkaeufer: "", prioritaet: "Mittel", zahlung: "Netto 30 Tage",
-        lieferadresse: "", bemerkungen: "", gesamtbetrag: "0,00 €",
-        positions: [{ mat: "", name: "", qty: 0, unit: "Stk", price: 0 }]
-      };
-    }
+    onInit: function () {
+      this._filterVisible = true;
 
-    return Controller.extend("purchaseorders.controller.List", {
+      this._oCreateCtrl = new CreateController();
+      this._oCreateCtrl.init(
+        this.getView(),
+        this.getOwnerComponent().getModel()
+      );
+    },
 
-      onInit: function () {
-        this._filterVisible = true;
-        this.getView().setModel(new JSONModel(emptyCreate()), "create");
-      },
+    onCreatePress: function () {
+      this._oCreateCtrl.openDialog();     // all Create logic is in Create.controller.js
+    },
 
-      formatStatusText:  function (sCode) { return STATUS_TEXT[sCode]  || sCode || "–"; },
-      formatStatusState: function (sCode) { return STATUS_STATE[sCode] || "None"; },
+    formatStatusText: function (sCode) { return STATUS_TEXT[sCode] || sCode || "–"; },
+    formatStatusState: function (sCode) { return STATUS_STATE[sCode] || "None"; },
 
-      formatAmount: function (sAmount, sCurrency) {
-        if (!sAmount) return "–";
-        var fVal = parseFloat(sAmount);
-        if (isNaN(fVal)) return sAmount;
-        return fVal.toLocaleString("de-DE", { minimumFractionDigits: 2, maximumFractionDigits: 2 })
-          + " " + (sCurrency || "");
-      },
+    formatAmount: function (sAmount, sCurrency) {
+      if (!sAmount) { return "–"; }
+      var fVal = parseFloat(sAmount);
+      if (isNaN(fVal)) { return sAmount; }
+      return fVal.toLocaleString("de-DE", {
+        minimumFractionDigits: 2, maximumFractionDigits: 2
+      }) + " " + (sCurrency || "");
+    },
 
-      onTableUpdateFinished: function (oEvent) {
-        var iTotal  = oEvent.getParameter("total");
-        var iActual = oEvent.getParameter("actual");
-        this.byId("txtFilterCount").setText(iActual + " von " + iTotal + " Bestellungen");
-      },
+    onTableUpdateFinished: function (oEvent) {
+      var iTotal  = oEvent.getParameter("total");
+      var iActual = oEvent.getParameter("actual");
+      this.byId("txtFilterCount").setText(iActual + " von " + iTotal + " Bestellungen");
+    },
 
-      onToggleFilter: function () {
-        this._filterVisible = !this._filterVisible;
-        this.byId("filterPanel").setVisible(this._filterVisible);
-        this.byId("btnToggleFilter").setText(
-          this._filterVisible ? "Filter ausblenden" : "Filter einblenden"
-        );
-      },
+    onOrderPress: function (oEvent) {
+      var sId = oEvent.getSource().getBindingContext().getProperty("PurchaseOrder");
+      this.getOwnerComponent().getRouter().navTo("detail", { id: sId });
+    },
 
-      onLiveSearch: function () { this._applyFilters(); },
-      onSearch:     function () { this._applyFilters(); },
-      onFilter:     function () { this._applyFilters(); },
+    onToggleFilter: function () {
+      this._filterVisible = !this._filterVisible;
+      this.byId("filterPanel").setVisible(this._filterVisible);
+      this.byId("btnToggleFilter").setText(
+        this._filterVisible ? "Filter ausblenden" : "Filter einblenden"
+      );
+    },
 
-      _applyFilters: function () {
-        var sSearch   = this.byId("searchField").getValue().trim();
-        var sStatus   = this.byId("filterStatus").getSelectedKey();
-        var sPurchOrg = this.byId("filterPurchOrg").getSelectedKey();
-        var sCompany  = this.byId("filterCompany").getSelectedKey();
+    onLiveSearch: function () { this._applyFilters(); },
+    onSearch:     function () { this._applyFilters(); },
+    onFilter:     function () { this._applyFilters(); },
 
-        var aFilters = [];
+    _applyFilters: function () {
+      var sSearch   = this.byId("searchField").getValue().trim();
+      var sStatus   = this.byId("filterStatus").getSelectedKey();
+      var sPurchOrg = this.byId("filterPurchOrg").getSelectedKey();
+      var sCompany  = this.byId("filterCompany").getSelectedKey();
+      var aFilters  = [];
 
-        if (sSearch) {
-          aFilters.push(new Filter({
-            filters: [
-              new Filter("PurchaseOrder", FilterOperator.Contains, sSearch),
-              new Filter("Supplier",      FilterOperator.Contains, sSearch),
-            ],
-            and: false
-          }));
-        }
-        if (sStatus)   aFilters.push(new Filter("PurchasingProcessingStatus", FilterOperator.EQ, sStatus));
-        if (sPurchOrg) aFilters.push(new Filter("PurchasingOrganization",     FilterOperator.EQ, sPurchOrg));
-        if (sCompany)  aFilters.push(new Filter("CompanyCode",                FilterOperator.EQ, sCompany));
-
-        this.byId("ordersTable").getBinding("items").filter(
-          aFilters.length ? new Filter({ filters: aFilters, and: true }) : []
-        );
-      },
-
-      onOrderPress: function (oEvent) {
-        var sId = oEvent.getSource().getBindingContext().getProperty("PurchaseOrder");
-        this.getOwnerComponent().getRouter().navTo("detail", { id: sId });
-      },
-
-      // CREATE DIALOG handlers
-      onCreatePress: function () {
-        this.getView().getModel("create").setData(emptyCreate());
-        this.byId("createDialog").open();
-      },
-
-      onCancelCreate: function () {
-        this.byId("createDialog").close();
-      },
-
-      onAddPosition: function () {
-        var oModel = this.getView().getModel("create");
-        var aPos = oModel.getProperty("/positions");
-        aPos.push({ mat: "", name: "", qty: 0, unit: "Stk", price: 0 });
-        oModel.setProperty("/positions", aPos);
-      },
-
-      onRemovePosition: function (oEvent) {
-        var oModel = this.getView().getModel("create");
-        var aPos = oModel.getProperty("/positions");
-        var iIdx = parseInt(oEvent.getSource().getBindingContext("create").getPath().split("/").pop(), 10);
-        aPos.splice(iIdx, 1);
-        oModel.setProperty("/positions", aPos);
-        this.onPositionChange();
-      },
-
-      onPositionChange: function () {
-        var oModel = this.getView().getModel("create");
-        var aPos = oModel.getProperty("/positions");
-        var total = 0;
-        aPos.forEach(function(p) {
-          total += (parseFloat(p.qty) || 0) * (parseFloat(p.price) || 0);
-        });
-        oModel.setProperty("/gesamtbetrag", fmtEuro(total));
-      },
-
-      onSaveCreate: function () {
-        var oData = this.getView().getModel("create").getData();
-
-        if (!oData.lieferant || !oData.kategorie || !oData.bestelldatum ||
-            !oData.lieferdatum || !oData.einkaeufer || !oData.lieferadresse) {
-          MessageBox.error("Bitte alle Pflichtfelder ausfüllen.");
-          return;
-        }
-
-        MessageToast.show("Bestellung wurde erfolgreich erstellt!");
-        this.byId("createDialog").close();
-      },
-
-      onExportPress: function () {
-        var aItems = this.byId("ordersTable").getItems();
-        if (!aItems.length) {
-          MessageToast.show("Keine Daten zum Exportieren.");
-          return;
-        }
-        var sCSV = "Bestellnummer,Lieferant,Status,Einkaufsorg.,Buchungskreis,Währung,Gesamtbetrag\n";
-        aItems.forEach(function (oItem) {
-          var o = oItem.getBindingContext();
-          sCSV += [
-            o.getProperty("PurchaseOrder"),
-            o.getProperty("Supplier"),
-            STATUS_TEXT[o.getProperty("PurchasingProcessingStatus")] || o.getProperty("PurchasingProcessingStatus"),
-            o.getProperty("PurchasingOrganization"),
-            o.getProperty("CompanyCode"),
-            o.getProperty("DocumentCurrency"),
-            o.getProperty("PurgReleaseTimeTotalAmount")
-          ].join(",") + "\n";
-        });
-        var oLink = document.createElement("a");
-        oLink.href = URL.createObjectURL(new Blob([sCSV], { type: "text/csv;charset=utf-8;" }));
-        oLink.download = "bestellungen.csv";
-        oLink.click();
-        MessageToast.show("Export erfolgreich!");
+      if (sSearch) {
+        aFilters.push(new Filter({
+          filters: [
+            new Filter("PurchaseOrder", FilterOperator.Contains, sSearch),
+            new Filter("Supplier",      FilterOperator.Contains, sSearch)
+          ],
+          and: false
+        }));
       }
+      if (sStatus)   { aFilters.push(new Filter("PurchasingProcessingStatus", FilterOperator.EQ, sStatus));   }
+      if (sPurchOrg) { aFilters.push(new Filter("PurchasingOrganization",     FilterOperator.EQ, sPurchOrg)); }
+      if (sCompany)  { aFilters.push(new Filter("CompanyCode",                FilterOperator.EQ, sCompany));  }
 
-    });
-  }
-);
+      this.byId("ordersTable").getBinding("items").filter(
+        aFilters.length ? new Filter({ filters: aFilters, and: true }) : []
+      );
+    },
+
+    onExportPress: function () {
+      var aItems = this.byId("ordersTable").getItems();
+      if (!aItems.length) { MessageToast.show("Keine Daten zum Exportieren."); return; }
+
+      var sCSV = "Bestellnummer,Lieferant,Status,Einkaufsorg.,Buchungskreis,Währung,Gesamtbetrag\n";
+      aItems.forEach(function (oItem) {
+        var o = oItem.getBindingContext();
+        sCSV += [
+          o.getProperty("PurchaseOrder"),
+          o.getProperty("Supplier"),
+          STATUS_TEXT[o.getProperty("PurchasingProcessingStatus")] || o.getProperty("PurchasingProcessingStatus"),
+          o.getProperty("PurchasingOrganization"),
+          o.getProperty("CompanyCode"),
+          o.getProperty("DocumentCurrency"),
+          o.getProperty("PurgReleaseTimeTotalAmount")
+        ].join(",") + "\n";
+      });
+
+      var oLink = document.createElement("a");
+      oLink.href = URL.createObjectURL(new Blob([sCSV], { type: "text/csv;charset=utf-8;" }));
+      oLink.download = "bestellungen.csv";
+      oLink.click();
+      MessageToast.show("Export erfolgreich!");
+    }
+
+  });
+});
